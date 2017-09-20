e196308d98da78da252b4dd40512a33943543639
Micha Alt <mail@lean-stack.rocks>
2017-09-20 11:26:02 +0200

    Changed the app title.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,15 +19,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Developers Notebook'`, async(() => {
+  it(`should have as title 'Devbook'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Developers Notebook');
+    expect(app.title).toEqual('Devbook');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Developers Notebook');
+    expect(compiled.querySelector('h1').textContent).toContain('Devbook');
   }));
 });
